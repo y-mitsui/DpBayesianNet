54994463332791925194c7e398818d05cc4805f5
Y.Mitsui <dyfomg@gmail.com>
2014-03-15 22:44:02 +0900

    first release

--- dpBayesianNetwork.h
+++ dpBayesianNetwork.h
@@ -7,18 +7,39 @@
 #define Calloc(type,n) (type *)calloc(1,(n)*sizeof(type))
 
 typedef struct{
-	int numNode;
-	int *edge;
-	int *numValuePattern;
-	Associate ***CPT;
+	int numNode;		//確率変数の数
+	int *edge;		//ネットワーク構造
+	int *numValuePattern;	//各ノードごとの取りうる値の数
+	Associate ***CPT;	//条件付き確率表
 }bayesianNetwork;
 
 
+/* bayesianNetTrain
+動的計画法による学習
+data:サンプリングデータ  要素数はnumSample*dimention
+numSample:サンプリングデータの数
+dimention:サンプリングデータの次元(確率変数の数)
+numValuePattern:各ノードごとの取りうる値の数
+戻り値:学習結果
+*/
+bayesianNetwork* bayesianNetTrain(int *data,int numSample,int dimention,int *numValuePattern);
+
+/* bayesianNetPredict
+クラス分類を行う
+model:bayesianNetTrainによって得られたコンテキスト
+targetNode:目的変数
+values:説明変数の値。(エビデンス)
+戻り値:確率が最大となるtargetNodeが取る値
+*/
+int bayesianNetPredict(bayesianNetwork *model,int *values,int targetNode);
+
+/* bayesianNetFree
+学習結果のリリース */
+void bayesianNetFree(bayesianNetwork *model);
 
 int combination(int n,int m);
 void bayesianNetPrintSquereMatrix(int *matrix,int numMatrix);
-int bayesianNetPredict(bayesianNetwork *model,int *values,int targetNode);
-bayesianNetwork* bayesianNetTrain(int *data,int numSample,int dimention,int *numValuePattern);
-void bayesianNetFree(bayesianNetwork *model);
+
+
 
 #endif
