b5e4bc00e93479ea1688b37bc44cdb778e7d5c05
Y.Mitsui <dyfomg@gmail.com>
2014-03-15 21:31:49 +0900

    preparation release

---
+++ CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/example.dir/example.c.obj"
+  "CMakeFiles/example.dir/dpBayesianNet.c.obj"
+  "CMakeFiles/example.dir/laa.c.obj"
+  "CMakeFiles/example.dir/util.c.obj"
+  "example.pdb"
+  "example.exe"
+  "example.exe.manifest"
+  "libexample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
